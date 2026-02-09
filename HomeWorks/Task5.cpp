953b7645da5b61758e60350e2b4563eef63f758d
Gevorg <gevjan199755@gmail.com>
2026-02-10 02:41:23 +0400

    Task 5: new/delete vs malloc/free

---
+++ HomeWorks/Task5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std:: cout << "Enter size of array ";
+    std:: cin >> n ;
+    
+    int* arr = new int[n];
+
+    for(int i = 0; i < n; ++i){
+        std::cout << "Enter number" << i+1 << "/ ";
+        std:: cin >> arr[i]; 
+    }
+
+    for(int i = 0; i < n; ++i){
+        std::cout << arr[i] << " ";
+    }
+        std::cout<<std::endl;
+
+        delete[] arr;
+
+        return 0;
+    
+}
